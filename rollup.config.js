da888431ea87f7e5bb8aa498ec392dfd3054776d
mustib <mmi197here@gmail.com>
2026-03-10 23:14:37 +0200

    Edit package bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import { readdirSync, rmSync, statSync } from 'node:fs';
+import { rmSync } from 'node:fs';
 import path from 'node:path';
 import { getDirname } from '@mustib/utils/node';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
@@ -13,25 +13,42 @@ const outDir = path.join(__dirname, 'dist');
 
 rmSync(outDir, { recursive: true, force: true });
 
-const componentsEntries = readdirSync(componentsDir, {
-  recursive: true,
-  encoding: 'utf8',
-}).reduce(
-  (result, _path) => {
-    const parsed = path.parse(_path);
-    const fullPath = path.join(componentsDir, _path);
-    if (
-      statSync(fullPath).isFile() &&
-      parsed.name !== 'index' &&
-      parsed.ext === '.ts'
-    ) {
-      result[path.join('components', parsed.name)] = fullPath;
-    }
+/**
+ * Array of entry path relative to `componentsDir` and export path
+ */
+const exportedPaths = [
+  ['index.ts', 'index'],
+  ['mu-element.ts', 'components/mu-element'],
+  ['mu-icon.ts', 'components/mu-icon'],
+  ['mu-trigger.ts', 'components/mu-trigger'],
+  ['mu-transparent.ts', 'components/mu-transparent'],
+  ['mu-range/mu-range.ts', 'components/mu-range'],
+  ['mu-range/mu-range-fill.ts', 'components/mu-range-fill'],
+  ['mu-range/mu-range-thumb.ts', 'components/mu-range-thumb'],
+  ['mu-range/mu-range-thumb-value.ts', 'components/mu-range-thumb-value'],
+  ['mu-select/mu-select.ts', 'components/mu-select'],
+  ['mu-select/mu-select-item.ts', 'components/mu-select-item'],
+  ['mu-select/mu-select-items.ts', 'components/mu-select-items'],
+  ['mu-select/mu-select-label.ts', 'components/mu-select-label'],
+  [
+    'mu-select/mu-select-label-content.ts',
+    'components/mu-select-label-content',
+  ],
+  ['mu-sortable/mu-sortable.ts', 'components/mu-sortable'],
+  ['mu-sortable/mu-sortable-trigger.ts', 'components/mu-sortable-trigger'],
+  ['mu-sortable/mu-sortable-item.ts', 'components/mu-sortable-item'],
+  ['mu-toast/mu-toast.ts', 'components/mu-toast'],
+  ['mu-toast/mu-toast-item.ts', 'components/mu-toast-item'],
+  ['mu-toast/Toast.ts', 'utils/Toast'],
+  ['mu-toast/ToastController.ts', 'utils/ToastController'],
+];
+
+const componentsEntries = exportedPaths.reduce(
+  (result, [entry, exportPath]) => {
+    result[exportPath] = path.join(componentsDir, entry);
     return result;
   },
-  {
-    index: path.join(componentsDir, 'index.ts'),
-  },
+  {},
 );
 
 export default defineConfig([
